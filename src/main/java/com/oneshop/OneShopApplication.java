b79bda7efb9215e8b86e5fbf0eb441ae8e92e3aa
ntnhtc <han.ntnhtc@gmail.com>
2024-09-26 17:38:46 +0700

    configured sitemesh for layout

--- src/main/java/com/oneshop/OneShopApplication.java
+++ src/main/java/com/oneshop/OneShopApplication.java
@@ -2,6 +2,10 @@ package com.oneshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.oneshop.configs.CustomSiteMeshFilter;
 
 @SpringBootApplication
 public class OneShopApplication {
--- src/main/java/com/oneshop/ServletInitializer.java
+++ src/main/java/com/oneshop/ServletInitializer.java
@@ -1,8 +1,14 @@
 package com.oneshop;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import com.oneshop.configs.CustomSiteMeshFilter;
+
+@Configuration
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
@@ -10,4 +16,12 @@ public class ServletInitializer extends SpringBootServletInitializer {
 		return application.sources(OneShopApplication.class);
 	}
 
+
+    @Bean
+    FilterRegistrationBean<CustomSiteMeshFilter> siteMeshFilter() {
+        FilterRegistrationBean<CustomSiteMeshFilter> filter = new FilterRegistrationBean<CustomSiteMeshFilter>();
+        filter.setFilter(new CustomSiteMeshFilter());
+        filter.addUrlPatterns("/*");
+        return filter;
+    }
 }
